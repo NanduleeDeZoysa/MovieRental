12f8d78e04c27317e476277e9f6a629a8bcf315d
IT24100387 <it24100387@my.sliit.lk>
2025-05-09 23:03:05 +0530

    Create the user list

--- src/main/java/com/example/movierental/Controller.java
+++ src/main/java/com/example/movierental/Controller.java
@@ -1,16 +1,22 @@
 package com.example.movierental;
 
 import com.example.movierental.model.User;
+import com.example.movierental.model.UserNode;
 import com.example.movierental.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @Autowired
     UserServices userServices;
+    private static final String UPLOAD_DIR = "uploads/";
 
     @GetMapping("/")
     public String index(Model model) {
@@ -29,18 +35,26 @@ public class Controller {
     }
 
     @GetMapping("/profile")
-    public String profile() {
+    public String profile(@RequestParam String username, Model model) {
+        model.addAttribute("username", username);
+        String name = userServices.getUserName(username);
+        model.addAttribute("name", name);
+        String email = userServices.getEmail(username);
+        model.addAttribute("email", email);
+        String number = userServices.getNumber(username);
+        model.addAttribute("number", number);
         return "profile";
     }
 
     @PostMapping("/registration")
     //@ResponseBody
-    public String registration(Model model, @RequestParam String name, @RequestParam String email, @RequestParam String password, @RequestParam String repeatPassword) {
+    public String registration(Model model,
+                               @RequestParam String name, @RequestParam String email, @RequestParam String number, @RequestParam String password, @RequestParam String repeatPassword) {
         if (!password.equals(repeatPassword)) {
             model.addAttribute("message2", "Passwords do not match");
            return "registration";
        }
-        User u1 = new User(name, email, password);
+        UserNode u1 = new UserNode(name, email, number, password);
         if(userServices.Adduser(u1))
             return "login";
         else {
@@ -50,13 +64,81 @@ public class Controller {
     }
 
     @PostMapping("/login")
-    public String login(Model model, @RequestParam String name, @RequestParam String password){
-        if(userServices.LoginUser(name, password))
-            return "profile";
+    public String login(Model model, @RequestParam String name, @RequestParam String password) {
+        if (userServices.LoginUser(name, password))
+            return "redirect:/profile?username=" + name;
         else {
             model.addAttribute("message3", "Invalid username or password");
             return "login";
         }
     }
+
+    @GetMapping("/EditProfile")
+    public String editProfile(@RequestParam String username, Model model) {
+        model.addAttribute("username", username);
+        return "EditProfile";
+    }
+
+    @PostMapping("/saveprofile")
+    public String saveProfile(Model model, @RequestParam String username , @RequestParam String email,
+                              @RequestParam String number, @RequestParam String oldPassword ,
+                              @RequestParam String newPassword , @RequestParam String repeatPassword) {
+
+        if (userServices.LoginUser(username, oldPassword)) {
+            if (newPassword.equals(repeatPassword)) {
+                UserNode u1 = new UserNode(username, email, number, newPassword);
+                if(userServices.updateUser(username,u1)){
+                    return "login";
+                }else{
+                    model.addAttribute("username", username);
+                    model.addAttribute("message2", "User update failed");
+                    return "/EditProfile";
+                }
+            }
+            else {
+                model.addAttribute("username", username);
+                model.addAttribute("message2", "Passwords do not match");
+                return "/EditProfile";
+            }
+        }
+        else {
+            model.addAttribute("username", username);
+            model.addAttribute("message1", "Invalid Old password");
+            return "EditProfile";
+        }
+
+    }
+
+    @GetMapping("/EditProfileImage")
+    public String editProfileImage(@RequestParam String username, Model model) {
+        model.addAttribute("username", username);
+        return "uploadImage";
+    }
+
+
+    @PostMapping("/uploadImage")
+    public String uploadImage( Model model,@RequestParam String username,
+                               @RequestParam("image") MultipartFile imageFile) {
+
+        model.addAttribute("username", username);
+        if (imageFile.isEmpty()) {
+            model.addAttribute("message1", "No image file selected") ;
+        }
+        try {
+            // Save the file with original filename
+            String filePath = imageFile.getOriginalFilename();
+            File dir = new File(username);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            File dest = new File(username+".jpg");
+            imageFile.transferTo(dest);
+
+        } catch (IOException e) {
+            model.addAttribute("message1", "Failed to save image") ;
+        }
+    return "profile";
+    }
+
 }
 
--- src/main/java/com/example/movierental/model/User.java
+++ src/main/java/com/example/movierental/model/User.java
@@ -3,13 +3,15 @@ package com.example.movierental.model;
 public class User {
     protected String name;
     protected String email;
+    protected String number;
     public String password;
     public String repeatPassword;
 
 
-    public User( String name, String email, String password) {
+    public User( String name, String email, String number, String password) {
         this.name = name;
         this.email = email;
+        this.number = number;
         this.password = password;
         this.repeatPassword = password;
     }
@@ -17,24 +19,33 @@ public class User {
     public User() {
         this.name = "";
         this.email = "";
+        this.number = "";
         this.password = "";
     }
 
+    //Getters
     public String getName() {
         return name;
     }
     public String getEmail() {
         return email;
     }
+    public String getNumber() {
+        return number;
+    }
     public String getPassword() {
         return password;
     }
 
+    //Setters
     public void setName(String name) {
         this.name = name;
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+    public void setNumber(String number) {
+        this.number = number;
     }
     public void setPassword(String password) {
         this.password = password;
